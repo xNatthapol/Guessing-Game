00800fb699cce7275fa76b044388692dac06cb41
Natthapol SERMSARAN <natthapolwork@gmail.com>
2025-04-04 09:00:32 +0700

    Add config for CORS Middleware

--- backend/internal/routes/api.go
+++ backend/internal/routes/api.go
@@ -1,6 +1,7 @@
 package routes
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -8,6 +9,14 @@ var router = gin.Default()
 
 // Run will start the server
 func Run() {
+	// Configure CORS middleware
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+	}))
+
 	getRoutes()
 	router.Run(":8080")
 }
